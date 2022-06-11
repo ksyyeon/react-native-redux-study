e228f26df14632a12efbdaba3f15c45dc051a8ef
ksyyeon <suyeonmin@naver.com>
2022-06-11 18:53:30 +0900

    react-redux 적용

--- Countly/App.js
+++ Countly/App.js
@@ -1,54 +1,52 @@
 import React, {Component, useCallback, useEffect, useState} from 'react';
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
-import {increment, decrement, zero} from './src/action';
+import {Provider, connect} from 'react-redux';
+import * as actions from './src/action';
 import store from './src/store';
+import Counter from './src/Counter';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.updateState = this.updateState.bind(this);
-    this.state = {
-      tally: store.getState(),
-      unsubscribe: store.subscribe(this.updateState),
-    };
-  }
+const mapStateToProps = state => ({
+  count: state.count,
+});
 
-  componentDidMount() {}
+const CounterContainer = connect(mapStateToProps, actions)(Counter);
 
-  componentWillUnmount() {
-    this.state.unsubscribe();
-  }
+export default App = () => (
+  <Provider store={store}>
+    <CounterContainer />
+  </Provider>
+);
 
-  updateState() {
-    this.setState({
-      tally: store.getState(),
-    });
-  }
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.updateState = this.updateState.bind(this);
+//     this.state = {
+//       tally: store.getState(),
+//       unsubscribe: store.subscribe(this.updateState),
+//     };
+//   }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.appName}>Countly</Text>
-        <Text style={styles.tally}> Tally : {this.state.tally.count}</Text>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => store.dispatch(increment())}>
-          <Text style={styles.buttonText}>+</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => store.dispatch(decrement())}>
-          <Text style={styles.buttonText}>-</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => store.dispatch(zero())}>
-          <Text style={styles.buttonText}>0</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-}
+//   componentDidMount() {}
+
+//   componentWillUnmount() {
+//     this.state.unsubscribe();
+//   }
+
+//   updateState() {
+//     this.setState({
+//       tally: store.getState(),
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <Provider store={store}>
+//         <Counter />
+//       </Provider>
+//     );
+//   }
+// }
 
 // export default App = props => {
 //   const [tally, setTally] = useState(store.getState());
@@ -85,35 +83,3 @@ export default class App extends Component {
 //     </View>
 //   );
 // };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5fcff',
-  },
-
-  appName: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  tally: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 20,
-    fontSize: 25,
-  },
-  button: {
-    backgroundColor: 'blue',
-    width: 100,
-    marginBottom: 20,
-    padding: 20,
-  },
-  buttonText: {
-    color: 'white',
-    textAlign: 'center',
-    fontSize: 20,
-  },
-});
---
+++ Countly/src/Counter.js
@@ -0,0 +1,60 @@
+import React, {Component, useCallback, useEffect, useState} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import PropTypes from 'prop-types';
+
+const Counter = props => (
+  <View style={styles.container}>
+    <Text style={styles.appName}>Countly</Text>
+    <Text style={styles.tally}> Tally : {props.count}</Text>
+    <TouchableOpacity style={styles.button} onPress={props.increment}>
+      <Text style={styles.buttonText}>+</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.button} onPress={props.decrement}>
+      <Text style={styles.buttonText}>-</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.button} onPress={props.zero}>
+      <Text style={styles.buttonText}>0</Text>
+    </TouchableOpacity>
+  </View>
+);
+
+Counter.propTypes = {
+  count: PropTypes.number,
+  increment: PropTypes.func,
+  decrement: PropTypes.func,
+  zero: PropTypes.func,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5fcff',
+  },
+
+  appName: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  tally: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 20,
+    fontSize: 25,
+  },
+  button: {
+    backgroundColor: 'blue',
+    width: 100,
+    marginBottom: 20,
+    padding: 20,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 20,
+  },
+});
+
+export default Counter;
